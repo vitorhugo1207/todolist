dd14d2240fb63e1d19183c2627502b2bc53c6bda
vitorhugo1207 <vitorhugoalvesferreira@outlook.com>
2023-10-12 11:08:00 -0300

    setting database

--- src/main/java/com/vitor/todolist/user/User.java
+++ src/main/java/com/vitor/todolist/user/User.java
@@ -1,13 +1,31 @@
 package com.vitor.todolist.user;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
-@Data // add getters and setters
+@Data // add getters and setters 
+@Entity(name = "tb_users")
 public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
     private String username;
     private String name;
     private String password;
+
+    // Store when data was created
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
     // Constructors
     public User() {
