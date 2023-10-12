507687b8ef3e43bba30592e8308f1e71f6e4b42a
vitorhugo1207 <vitorhugoalvesferreira@outlook.com>
2023-10-12 13:55:41 -0300

    Added Response Entity

--- src/main/java/com/vitor/todolist/task/TaskController.java
+++ src/main/java/com/vitor/todolist/task/TaskController.java
@@ -1,5 +1,30 @@
 package com.vitor.todolist.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/task")
 public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+
+        if (taskModel.getTitle().length() > 50) {
+            System.out.println("Tittle larger than 50 characters");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tittle larger than 50 characters");
+        }
+
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    }
     
 }
