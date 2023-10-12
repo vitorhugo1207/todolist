c1030cc18b48ba13ca900a243037830aaae501e8
vitorhugo1207 <vitorhugoalvesferreira@outlook.com>
2023-10-12 15:36:46 -0300

    Added routes requests filters

--- src/main/java/com/vitor/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/vitor/todolist/filter/FilterTaskAuth.java
@@ -2,21 +2,13 @@ package com.vitor.todolist.filter;
 
 import java.io.IOException;
 import java.util.Base64;
-
-import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import com.vitor.todolist.user.UserRepository;
-
 import at.favre.lib.crypto.bcrypt.BCrypt;
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -33,8 +25,8 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
             
-        var servletPath = request.getServletPath();
-        if (servletPath.equals("/task/")) {
+        var servletPath = request.getServletPath(); // Get route requested 
+        if (servletPath.equals("/task/")) { // if Requested Route if equals to /task/
             var authorization = request.getHeader("Authorization"); // Get value auth 
     
             var authEncoded = authorization.substring(5).trim(); // trim() remove spaces
@@ -51,6 +43,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
             } else {
                 var resultPassword = BCrypt.verifyer().verify(credentials[1].toCharArray(), user.getPassword()).verified;
                 if (resultPassword) {
+                    request.setAttribute("idUser", user.getId()); // set a attribute to get in taskController
                     filterChain.doFilter(request, response);
                 } else {
                     response.sendError(401);
