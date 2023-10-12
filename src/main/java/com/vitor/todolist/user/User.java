cafab3e7eaecb8cc74b32d4b9e69c59ccc39101b
vitorhugo1207 <vitorhugoalvesferreira@outlook.com>
2023-10-12 10:33:31 -0300

    Added Lombok to User

--- src/main/java/com/vitor/todolist/user/User.java
+++ src/main/java/com/vitor/todolist/user/User.java
@@ -1,7 +1,10 @@
 package com.vitor.todolist.user;
 
+import lombok.Data;
+
+@Data // add getters and setters
 public class User {
-    
+
     private String username;
     private String name;
     private String password;
@@ -9,30 +12,11 @@ public class User {
     // Constructors
     public User() {
     }
+
     public User(String username, String name, String password) {
         this.username = username;
         this.name = name;
         this.password = password;
     }
-
-    // Getters and Setters
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+    
 }
